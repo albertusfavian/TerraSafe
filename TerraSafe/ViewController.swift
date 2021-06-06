a8812b0e0dc7acf710266cd45ba914029eff9982
Albertus Favian <favianalbert@gmail.com>
2021-06-06 21:51:48 +0700

    Initial Commit

---
+++ TerraSafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TerraSafe
+//
+//  Created by Mac-albert on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
